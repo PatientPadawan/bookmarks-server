718ad7cbb0372f637af5c42f5476ec272b3eb3a4
Patient Padawan <1PatientPadawan@gmail.com>
2019-11-07 17:52:54 -0700

    refactored POST and DELETE functions to work with DB

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,11 +1,18 @@
 const express = require('express')
-const uuid = require('uuid')
+const xss = require('xss')
 const logger = require('../logger')
-const bookmarks = require('../store')
 const BookmarksService = require('./bookmarks-service')
 
 const bkmarkRouter = express.Router()
 const bodyParser = express.json()
+
+const xssBookmark = bookmark => ({
+    id: bookmark.id,
+    title: xss(bookmark.title),
+    url: bookmark.url,
+    description: xss(bookmark.description),
+    rating: Number(bookmark.rating),
+})
 
 bkmarkRouter
     .route('/bookmarks')
@@ -13,104 +20,81 @@ bkmarkRouter
         const knexInstance = req.app.get('db')
         BookmarksService.getAllBookmarks(knexInstance)
             .then(bookmarks => {
-                res.json(bookmarks)
+                res.json(bookmarks.map(xssBookmark))
             })
             .catch(next)
     })
-    .post(bodyParser, (req, res) => {
+    .post(bodyParser, (req, res, next) => {
+        for (const field of ['title', 'url', 'rating']) {
+            if (!req.body[field]) {
+                logger.error(`${field} is required`)
+                return res
+                    .status(400)
+                    .send({error: {message: `'${field}' is required`}
+                })
+            }
+        }
+
         const { title, url, rating, description } = req.body;
 
-        if (!title) {
-            logger.error(`Title is required`);
-            return res
-                .status(400)
-                .send('Invalid data');
-        }
-    
-        if (!url) {
-            logger.error(`Url is required`);
-                return res
-                    .status(400)
-                    .send(`Invalid data`);
-        }
-    
-        if (!rating) {
-            logger.error(`Rating is required`);
-                return res
-                    .status(400)
-                    .send(`Invalid data`);
-        }
     
         const numRating = parseFloat(rating)
     
-        if (Number.isNaN(numRating)) {
-            logger.error(`Rating must be numeric`);
-                return res
-                    .status(400)
-                    .send(`Invalid data`);
-        }
+        if (Number.isNaN(numRating) || numRating < 0 || numRating > 5) {
+            logger.error(`Invalid rating '${rating}' supplied`);
+            return res
+                .status(400)
+                .send({error: {message: `'rating' must be a number between 0 and 5`}
+            })
+        }    
     
-        if (!description) {
-            logger.error(`Description is required`);
-                return res
-                    .status(400)
-                    .send(`Invalid data`);
-        }
+        const newBookmark = { title, url, rating, description }
     
-        const id = uuid();
-    
-        const bookmark = {
-            id,
-            title,
-            url,
-            rating,
-            description
-        }
-    
-        bookmarks.push(bookmark)
-        logger.info(`Bookmark with id ${id} created`);
-    
-        res
-            .status(201)
-            .location(`http://localhost:8000/bookmark/${id}`)
-            .json(bookmark);
+        BookmarksService.insertBookmark(
+            req.app.get('db'),
+            newBookmark
+        )
+        .then(bookmark => {
+            logger.info(`Bookmark with id ${bookmark.id} created.`)
+            res
+                .status(201)
+                .location(`/bookmarks/${bookmark.id}`)
+                .json(xssBookmark(bookmark))
+        })
+    .catch(next)
 })
 
 bkmarkRouter
     .route('/bookmarks/:bookmark_id')
-    .get((req, res, next) => {
-        const knexInstance = req.app.get('db')
+    .all((req, res, next) => {
         const { bookmark_id } = req.params
-        BookmarksService.getById(knexInstance, bookmark_id)
+        BookmarksService.getById(req.app.get('db'), bookmark_id)
         .then(bookmark => {
-          if (!bookmark) {
+            if (!bookmark) {
             logger.error(`Bookmark with id ${bookmark_id} not found.`)
             return res.status(404).json({
-              error: { message: `Bookmark Not Found` }
+                error: { message: `Bookmark not found` }
             })
-          }
-          res.json(bookmark)
+            }
+            res.bookmark = bookmark
+            next()
         })
         .catch(next)
     })
-    .delete((req, res) => {
-        const { id } = req.params
-
-        const bkmarkIndex = bookmarks.findIndex(b => b.id == id);
-    
-        if (bkmarkIndex === -1) {
-            logger.error(`Bookmark with id ${id} not found.`);
-            return res
-                .status(404)
-                .send('Not found');
-        }
-    
-        bookmarks.splice(bkmarkIndex, 1);
-        logger.info(`Bookmark with id ${id} deleted.`);
-    
-        res
-            .status(204)
-            .end();
+    .get((req, res) => {
+       res.json(xssBookmark(res.bookmark))
+    })
+    .delete((req, res, next) => {
+        const { bookmark_id } = req.params
+        BookmarksService.deleteBookmark(
+            req.app.get('db'),
+            bookmark_id
+        )
+        .then(rowsDeleted => {
+            logger.info(`Bookmark with id ${bookmark_id} deleted.`)
+            res.status(204).end()
+        })
+        .catch(next)
 })
 
 module.exports = bkmarkRouter
